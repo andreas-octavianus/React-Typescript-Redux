d249db2f55303bca3935bab88b81b268c77c73e3
Andreas Octavianus <andreas.octavianus@manta-tm.com>
2017-07-01 02:43:24 +0700

    set project with redux and typescript

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import './App.css';
+
+import Hello from './containers/Hello';
+import {createStore} from 'redux';
+import {enthusiasm} from './reducers/index';
+import {StoreState} from './types/index';
+import {Provider} from 'react-redux';
+
+const logo = require('./logo.svg');
+
+const store = createStore<StoreState>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript'
+});
+
+class App extends React.Component<{}, {}> {
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="App">
+                    <div className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h2>Welcome to React</h2>
+                    </div>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.tsx</code> and save to reload.
+                        <Hello />
+                    </p>
+                </div>
+            </Provider>
+        );
+    }
+}
+
+export default App;
