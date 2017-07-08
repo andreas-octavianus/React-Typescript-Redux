f2d5fdfa43f27f66c667a061147b2dc8d7871b63
Andreas Octavianus <andreas.octavianus@manta-tm.com>
2017-07-09 04:20:09 +0700

    Sync form with validate

---
+++ src/module/identity/component/IdentityForm.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import {Component} from 'react';
+import {Field, FormErrors, FormProps, FormWarnings, reduxForm} from 'redux-form';
+import {Identity} from '../type/Identity';
+
+interface IdentityFormProps {
+}
+
+interface IdentityFormState {
+}
+
+const validate = (values: Identity) => {
+    const errors: FormErrors<Identity> = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    } else if (values.firstName.length < 3) {
+        errors.firstName = 'Must be 3 characters or more';
+    }
+    return errors;
+};
+
+const warn = (values: Identity) => {
+    const warnings: FormWarnings<Identity> = {};
+    if (values.firstName === 'John') {
+        warnings.firstName = 'John is already taken';
+    }
+    return warnings;
+};
+
+class IdentityForm extends Component<
+    IdentityFormProps &
+    FormProps<Identity, any, any>, IdentityFormState> {
+
+    renderForm({input, meta, placeholder}: any) {
+        return (
+            <div className="input-row">
+                <input type="text" {...input} placeholder={placeholder}/>
+                <div>
+                    {meta.touched && meta.error && <span className="error">{meta.error}</span>}
+                    {meta.touched && meta.warning && <span className="warning">{meta.warning}</span>}
+                </div>
+            </div>
+        );
+    }
+
+    render() {
+        const {pristine, submitting, reset} = this.props;
+        return (
+            <form>
+                <div>
+                    <label>First Name</label>
+                    <div>
+                        <Field name="firstName" component={this.renderForm} placeholder="First Name"/>
+                    </div>
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <div>
+                        <Field name="lastName" component={this.renderForm} placeholder="Last Name"/>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" disabled={pristine || submitting}>Submit</button>
+                    <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+                </div>
+            </form>
+        );
+    }
+
+}
+
+export default reduxForm({
+    form: 'simple',
+    validate,
+    warn
+})(IdentityForm);
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,12 @@
 import {createStore, combineReducers} from 'redux';
 import {enthusiasm} from '../module/enthusiasm/reducer';
 import {todoReducer} from '../module/todo/reducer';
+import {reducer} from 'redux-form';
 
 export const appReducer = combineReducers({
     enthusiasm,
-    todoReducer
+    todoReducer,
+    form: reducer
 });
 
 export const store = createStore(appReducer);
